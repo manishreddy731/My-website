6dd4523b7eb874c72db117b8b478879334ddc48c
manishreddy731 <112796686+manishreddy731@users.noreply.github.com>
2025-04-24 04:29:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/My-website/', 
+  base: '/My-website/', // 👈 Important for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
